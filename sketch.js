681ea9afe1493e11a666f84f8ce0f3dc9f12abf7
Ayushi Singh <78640408+AyushiSingh9450@users.noreply.github.com>
2021-03-25 13:56:29 +0530

    Created Library and added BounceOff ALGORITHMS

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
 var moving, fixed;
-
+var rect1, rect2, rect3, rect4;
 
 function setup() {
   createCanvas(600,600);
-  moving = createSprite(300, 300, 40, 60);
+  moving = createSprite(100, 300, 40, 60);
+  moving.velocityX = 3;
+
   fixed = createSprite(300, 300, 60, 60);
+
+  rect1 = createSprite(100, 200, 50, 50);
+  rect2 = createSprite(200, 200, 50, 50);
+  rect3 = createSprite(300, 200, 50, 50);
+  rect4 = createSprite(400, 200, 50, 50);
 
   moving.shapeColor = "red";
   fixed.shapeColor = "white";
@@ -13,19 +20,52 @@ function setup() {
 function draw() {
   background(0);  
 
-  moving.x = mouseX;
-  moving.y = mouseY;
+  // moving.x = mouseX;
+  // moving.y = mouseY;
 
-  if (moving.x - fixed.x < moving.width/2+fixed.width/2 &&
-    fixed.x - moving.x < moving.width/2 + fixed.width/2 &&
-    fixed.y - moving.y < moving.height/2 + fixed.height/2 &&
-    moving.y - fixed.y < moving.height/2 + fixed.height/2) {
+  // arguments
+  if(isTouching(moving, fixed))  {
     moving.shapeColor = "blue";
-  fixed.shapeColor = "green";
+    fixed.shapeColor = "green";
   } else  {
     moving.shapeColor = "red";
     fixed.shapeColor = "white";
   }
 
+  if(isTouching(moving, rect1))  {
+    moving.shapeColor = "orange";
+    rect1.shapeColor = "orange";
+  } else  {
+    moving.shapeColor = "red";
+    rect1.shapeColor = "yellow";
+  }
+
+  if(isTouching(moving, rect2))  {
+    moving.shapeColor = "orange";
+    rect2.shapeColor = "orange";
+  } else  {
+    moving.shapeColor = "red";
+    rect2.shapeColor = "yellow";
+  }
+
+  if(isTouching(moving, rect3))  {
+    moving.shapeColor = "orange";
+    rect3.shapeColor = "orange";
+  } else  {
+    moving.shapeColor = "red";
+    rect3.shapeColor = "yellow";
+  }
+
+  if(isTouching(moving, rect4))  {
+    moving.shapeColor = "orange";
+    rect4.shapeColor = "orange";
+  } else  {
+    moving.shapeColor = "red";
+    rect4.shapeColor = "yellow";
+  }
+
+  bounceoff(moving,fixed);
+
   drawSprites();
 }
+
